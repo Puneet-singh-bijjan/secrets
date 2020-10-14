5aa4e62b2dd75e75aaa81a22011d96534893d08d
abc <abc@example.com>
2020-10-14 17:36:04 +0530

    added level 3 security with hashing function

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //jshint esversion:6
-require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const { stringify } = require("querystring");
 const encryption = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -19,8 +20,6 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
-
-userSchema.plugin(encryption , {secret : process.env.SECRET , encryptedFields : ['password']});
 
 const User = mongoose.model("User" , userSchema);
 
@@ -39,7 +38,7 @@ app.get("/register" , function(req , res){
 app.post("/register" , function(req , res ){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if(err){
@@ -52,7 +51,7 @@ app.post("/register" , function(req , res ){
 
 app.post("/login" , function(req ,res){
     const userName = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email: userName} , function(err , foundUser){
         if(err){
             res.send(err);
