9d8b661b110c3bf06deb1bbf44f5af68e61b712b
abc <abc@example.com>
2020-10-14 16:31:46 +0530

    added environment variables

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -20,9 +20,7 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 
-const secret = "jasmeetKaurSikhi";
-
-userSchema.plugin(encryption , {secret : secret , encryptedFields : ['password']});
+userSchema.plugin(encryption , {secret : process.env.SECRET , encryptedFields : ['password']});
 
 const User = mongoose.model("User" , userSchema);
 
